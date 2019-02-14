e0f22349194108c6f32c1de612b4126d1f6e288f
RayapinA <rayapin.antoine36@gmail.com>
2019-02-14 11:49:16 +0100

    Fonction pour la creation d'un admin

--- src/Command/CreateAdminCommand.php
+++ src/Command/CreateAdminCommand.php
@@ -2,22 +2,36 @@
 
 namespace App\Command;
 
+use App\Entity\User;
+use App\Manager\UserManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class CreateAdminCommand extends Command
 {
     protected static $defaultName = 'app:create-admin';
+    private $userManager;
+    private $encoder;
 
+    public function __construct(UserManager $userManager, UserPasswordEncoderInterface $encoder){
+
+        $this->userManager = $userManager;
+        $this->encoder = $encoder;
+        parent::__construct();
+
+    }
     protected function configure()
     {
         $this
-            ->setDescription('Add a short description for your command')
-            ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
+            ->setDescription('Command for create a new Admin')
+            ->addArgument('email', InputArgument::REQUIRED, 'email ')
+            ->addArgument('password', InputArgument::REQUIRED, 'paswword ')
             ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
         ;
     }
@@ -25,16 +39,21 @@ class CreateAdminCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $io = new SymfonyStyle($input, $output);
-        $arg1 = $input->getArgument('arg1');
+        $email = $input->getArgument('email');
+        $password = $input->getArgument('password');
+        $io->note(sprintf('You passed an argument: %s', $email));
 
-        if ($arg1) {
-            $io->note(sprintf('You passed an argument: %s', $arg1));
-        }
+        $user = new User();
+        $user->setEmail($email);
+        $passwordEncoded = $this->encoder->encodePassword($user, $password);
+        $user->setPassword($passwordEncoded);
+        $user->setRoles(['ROLE_USER','ROLE_ADMIN']);
+        $user->setName('LastName_Admin');
+        $user->setFirstName('FirstName_Admin');
 
-        if ($input->getOption('option1')) {
-            // ...
-        }
+        $this->userManager->save($user);
 
-        $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
+        $io->success(sprintf('You have created a User with email: %s',$email));
+
     }
 }
